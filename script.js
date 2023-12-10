3e8fda99827608bb8a17335d1b12fb7c798631d4
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-10 20:42:22 +0100

    script added to main inputs and buttons added

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+// This displays the current date
 var daty = new Date().toLocaleDateString()
 showDate.innerHTML = "Today's Date: "  + daty
+
+// This handles form submission
+function addBudget() {
+    if (item.value === "" || price.value === "") {
+        emptyField.style.display = "block"
+    } else {
+        
+    }
+}
