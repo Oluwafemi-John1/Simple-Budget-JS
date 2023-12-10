d2468491f6ec67115dcd28a647d00dce6844a57d
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-10 22:39:02 +0100

    modal added

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 // This displays the current date
 var daty = new Date().toLocaleDateString()
 showDate.innerHTML = "Today's Date: "  + daty
+showDateModal.innerHTML = "Today's Date: "  + daty
 
 // This handles form submission
 function addBudget() {
     if (item.value === "" || price.value === "") {
         emptyField.style.display = "block"
     } else {
-        
+
     }
 }
