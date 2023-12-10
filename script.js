1c58d661c23c63e64a7e21b859f73e512d9d9e7c
Oluwafemi Oyeniran <oyeniranoluwafemi36@gmail.com>
2023-12-10 22:47:52 +0100

    in progress

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// A global variable to set money available
+var money = 10000
+showMoney.innerHTML = "Available Money: " + money
+
 // This displays the current date
 var daty = new Date().toLocaleDateString()
 showDate.innerHTML = "Today's Date: "  + daty
 showDateModal.innerHTML = "Today's Date: "  + daty
+
+// This ensures the inputs only show after clicking a button and that button disappears after
+function showInputs() {
+    mainInputs.style.display = "block"
+    withdrawMoney.style.display = "none"
+}
 
 // This handles form submission
 function addBudget() {
